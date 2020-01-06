a68836ee5395258af036ba6d6bfe1d8a410a1f50
aotolbert@gmail.com <aotolbert@gmail.com>
2020-01-05 22:21:13 -0500

    front page scroll reactive images

--- app/app.js
+++ app/app.js
@@ -1,1 +1,46 @@
-console.log("I am an app")
+console.log("I am an app");
+
+
+let isInViewport = (elem) => {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top <= (window.innerHeight || document.documentElement.clientHeight) && 
+        bounding.bottom >= 0
+    );
+};
+
+
+let leftImages = document.querySelectorAll('.mainImgLeftContainerBefore');
+let rightImages = document.querySelectorAll('.mainImgRightContainerBefore');
+
+window.addEventListener('scroll', (evt) => {
+    leftImages.forEach(img => {
+        if(isInViewport(img)) {
+            console.log('Fading in the left image');
+            img.classList.add('fadeInLeftImage');
+            leftImages = document.querySelectorAll('.mainImgLeftContainerBefore');
+            img.addEventListener('animationend', () => {
+                img.classList.replace('mainImgLeftContainerBefore', 'mainImgLeftContainerAfter');
+
+            })
+        }
+    });
+
+    rightImages.forEach(image => {
+        if(isInViewport(image)) {
+            console.log('Fading in the right image');
+            image.classList.add('fadeInRightImage');
+            rightImages = document.querySelectorAll('.mainImgRightContainerBefore');
+            image.addEventListener('animationend', () => {
+                image.classList.replace('mainImgRightContainerBefore', 'mainImgRightContainerAfter');
+
+            })
+        }
+    })
+    
+
+})
+
+
+
+
