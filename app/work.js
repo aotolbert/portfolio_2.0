8022bd784622582be5dbce99ef166d88f123692c
Adam Tolbert <aotolbert@gmail.com>
2020-03-08 22:30:18 -0400

    preview essays close working and other small bugs

--- app/work.js
+++ app/work.js
@@ -8,55 +8,75 @@ Element.prototype.appendBefore = function (element) {
   element.parentNode.insertBefore(this, element);
 },false;
 
+
+
 var getWorkItemData = () => {
     //Query for the data
     var data = [
         {
             title: "title1",
-            imageTitle: "mona_lisa.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            imageTitle: "art_along_the_bounderies.jpg",
+            info: '"Art Along the Bounderies"',
+            link: "../essays/art_along_the_bounderies.pdf"
         },
         {
             title: "title2",
             imageTitle: "girl_with_a_pear_earring.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            info: '"Dust Storms in the Parallel Universe"',
+            link: "../essays/dust_storms_in_the_parallel_universe.pdf"
         },
         {
             title: "title3",
             imageTitle: "starry_night.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            info: '"Goodbye Georgia Blizzard"',
+            link: "../essays/goodbye_georgia_blizzard.pdf"
         },
         {
             title: "title4",
             imageTitle: "raymond_of_poitiers.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            info: '"High Art for New Millennium"',
+            link: "../essays/high_art_for_new_millennium.pdf"
         },
         {
             title: "title5",
             imageTitle: "mona_lisa.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            info: '"Images and Objects from Black Deep South"',
+            link: "../essays/images_and_objects_from_black_deep_south.pdf"
         },
         {
             title: "title6",
             imageTitle: "weeping_woman.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            info: '"Ingrained Images and Outside Influences"',
+            link: "../essays/ingrained_images_and_outside_influences.pdf"
         },
         {
             title: "title7",
             imageTitle: "girl_with_a_pear_earring.jpg",
-            info: "this is some text",
-            link: "../essays/look_at_that.pdf"
+            info: '"James B. Lemming"',
+            link: "../essays/james_b_lemming.pdf"
         },
         {
             title: "title8",
             imageTitle: "starry_night.jpg",
-            info: "this is some text",
+            info: '"Look At That"',
+            link: "../essays/look_at_that.pdf"
+        },
+        {
+            title: "title9",
+            imageTitle: "weeping_woman.jpg",
+            info: '"Paintbrush People and Bottlecap Thrones"',
+            link: "../essays/ingrained_images_and_outside_influences.pdf"
+        },
+        {
+            title: "title10",
+            imageTitle: "girl_with_a_pear_earring.jpg",
+            info: '"Paradise before and After the Fall"',
+            link: "../essays/james_b_lemming.pdf"
+        },
+        {
+            title: "title11",
+            imageTitle: "starry_night.jpg",
+            info: '"Revelations in Sleep Store and Wood"',
             link: "../essays/look_at_that.pdf"
         }
     ];
@@ -65,12 +85,20 @@ var getWorkItemData = () => {
 }
 
 function renderEssayPreviewDiv(essay) {
+
+    var existingPreview = document.querySelector('.previewEssay');
+    console.log(existingPreview);
+    if(existingPreview) {
+        var existingPreview = document.querySelector('.previewEssay').remove();
+    }
+
     var newDiv = document.createElement("div"); 
     newDiv.classList.add('previewEssay');
 
     let button = document.createElement('button');
     button.classList.add('closePreviewEssay');
-    button.innerText = "Close Preview";
+    button.innerText = "X";
+    button.onclick = closePreview;
     newDiv.appendChild(button);
     
     var embed = document.createElement("embed"); 
@@ -78,7 +106,7 @@ function renderEssayPreviewDiv(essay) {
     embed.type = "application/pdf";
     embed.width = "75%";
     embed.height = "75%";
-    embed.style= "margin-top: 10%; margin-left: 10%;";
+    embed.style= "margin-right: auto; margin-left: auto;";
     newDiv.appendChild(embed);  
 
     // document.getElementsByTagName("body")[0].classList.add('stop-scrolling');
@@ -123,4 +151,9 @@ function addElement () {
         document.querySelector('.workItemsContainer').appendChild(newDiv);
     });
 
+}
+
+function closePreview() {
+    console.log('closePreview() was clicked');
+    document.querySelector('.previewEssay').remove()
 }
